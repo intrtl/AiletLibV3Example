91e5cea732f86d3407e222f0e7d0c5544ae71a3d
Vsevolod Didkovskiy <vsevolod.didkovskiy@ailet.com>
2023-06-01 11:32:43 +0400

    Java example

---
+++ app/src/main/java/com/ailet/ailetv3test/MainActivityJava.java
@@ -0,0 +1,82 @@
+package com.ailet.ailetv3test;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import com.ailet.lib3.api.Ailet;
+import com.ailet.lib3.api.client.method.domain.start.AiletMethodStart;
+import java.util.ArrayList;
+
+public class MainActivityJava extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_java);
+
+
+        Button btInit = findViewById(R.id.btInitJava);
+        btInit.setOnClickListener(view -> {
+                    try {
+                        Ailet.getClient().init(
+                                "login",
+                                "password",
+                                null,
+                                false,
+                                null,
+                                false
+                        ).execute(
+                                result -> {
+
+                                    Ailet.getClient().start(
+                                            new AiletMethodStart.StoreId.External("storeId"),
+                                            "visitId",
+                                            null,
+                                            null,
+                                            null,
+                                            null,
+                                            null,
+                                            null,
+                                            null,
+                                            new ArrayList<>(),
+                                            new AiletMethodStart.LaunchConfig()
+                                    ).execute(result1 -> {
+                                                Log.i("Test", "Result");
+
+                                                return null;
+                                            },
+                                            throwable -> {
+                                                Log.e("Test", "Error");
+
+                                                return null;
+                                            },
+                                            () -> {
+                                                Log.i("test", "Complete");
+
+                                                return null;
+                                            });
+
+
+                                    return null;
+                                },
+                                throwable -> {
+                                    Log.e("Test", "Error");
+
+                                    return null;
+                                },
+                                () -> {
+                                    Log.i("test", "Complete");
+
+                                    return null;
+                                }
+                        );
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+
+        );
+    }
+}
