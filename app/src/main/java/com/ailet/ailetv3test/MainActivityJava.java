91871702a20200370645388adfd252f7846f06e8
Vsevolod Didkovskiy <vsevolod.didkovskiy@ailet.com>
2023-06-01 11:47:15 +0400

    Remove try-catch

--- app/src/main/java/com/ailet/ailetv3test/MainActivityJava.java
+++ app/src/main/java/com/ailet/ailetv3test/MainActivityJava.java
@@ -1,11 +1,14 @@
 package com.ailet.ailetv3test;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+
 import com.ailet.lib3.api.Ailet;
 import com.ailet.lib3.api.client.method.domain.start.AiletMethodStart;
+
 import java.util.ArrayList;
 
 public class MainActivityJava extends AppCompatActivity {
@@ -18,63 +21,58 @@ public class MainActivityJava extends AppCompatActivity {
 
         Button btInit = findViewById(R.id.btInitJava);
         btInit.setOnClickListener(view -> {
-                    try {
-                        Ailet.getClient().init(
-                                "login",
-                                "password",
-                                null,
-                                false,
-                                null,
-                                false
-                        ).execute(
-                                result -> {
+                    Ailet.getClient().init(
+                            "login",
+                            "password",
+                            null,
+                            false,
+                            null,
+                            false
+                    ).execute(
+                            result -> {
 
-                                    Ailet.getClient().start(
-                                            new AiletMethodStart.StoreId.External("storeId"),
-                                            "visitId",
-                                            null,
-                                            null,
-                                            null,
-                                            null,
-                                            null,
-                                            null,
-                                            null,
-                                            new ArrayList<>(),
-                                            new AiletMethodStart.LaunchConfig()
-                                    ).execute(result1 -> {
-                                                Log.i("Test", "Result");
+                                Ailet.getClient().start(
+                                        new AiletMethodStart.StoreId.External("storeId"),
+                                        "visitId",
+                                        null,
+                                        null,
+                                        null,
+                                        null,
+                                        null,
+                                        null,
+                                        null,
+                                        new ArrayList<>(),
+                                        new AiletMethodStart.LaunchConfig()
+                                ).execute(result1 -> {
+                                            Log.i("Test", "Result");
 
-                                                return null;
-                                            },
-                                            throwable -> {
-                                                Log.e("Test", "Error");
+                                            return null;
+                                        },
+                                        throwable -> {
+                                            Log.e("Test", "Error");
 
-                                                return null;
-                                            },
-                                            () -> {
-                                                Log.i("test", "Complete");
+                                            return null;
+                                        },
+                                        () -> {
+                                            Log.i("test", "Complete");
 
-                                                return null;
-                                            });
+                                            return null;
+                                        });
 
 
-                                    return null;
-                                },
-                                throwable -> {
-                                    Log.e("Test", "Error");
+                                return null;
+                            },
+                            throwable -> {
+                                Log.e("Test", "Error");
 
-                                    return null;
-                                },
-                                () -> {
-                                    Log.i("test", "Complete");
+                                return null;
+                            },
+                            () -> {
+                                Log.i("test", "Complete");
 
-                                    return null;
-                                }
-                        );
-
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
+                                return null;
+                            }
+                    );
                 }
 
         );
